a220bbd53df3fed82ac92d52846d90110608268e
SaaW008 <saad.achnani@gmail.com>
2023-11-11 11:09:07 -0800

    creation of .c files

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - prints the formated strings to standard output
+ * @format: formated string to handle
+ * @...: variadic arguments
+ * Return: todo
+ */
+
+int _printf(const char *format, ...)
+{
+
+	return (0);
+}
