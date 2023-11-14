1b11b796d513618d76195c2e2071e8743a8137ae
mBentaifour <bentaifourmoh@gmail.com>
2023-11-14 13:10:17 -0800

    update

---
+++ print_int.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+int itostdo(int i);
+
+/**
+ * printint - print int
+ * @n: pointer to int
+ * Return: number of element printed
+ */
+
+int printint(const int *n)
+{
+        int i_ret = 0;
+        int i = *n;
+        char signe;
+
+        if (i < 0)
+        {
+                signe = '-';
+                i_ret += write(1, &signe, 1);
+                i *= -1;
+                printint((int *)i);
+        }
+        else if (i < 10)
+        {
+                i_ret = itostdo(i);
+        }
+        else if (i >= 10)
+        {
+                i_ret = itostdo(i);
+        }
+        return (i_ret);
+}
+
+/**
+ * itostdo - print int to standard output stream
+ * @i: int
+ * Return: number of element printed
+ */
+
+int itostdo(int i)
+{
+        int itos_ret =0;
+        char tmp;
+        if( i > 9 )
+        {
+                int a;
+                a = i / 10;
+
+                i -= 10 * a;
+                itostdo(a);
+        }
+        tmp = '0' + i;
+        itos_ret = write(1, &tmp, 1);
+        return (itos_ret);
+}
---
+++ print_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * printstring - print string
+ * @c: pointer to string
+ * Return: number of element printed
+ */
+
+int printstring(const char *c)
+{
+        int s_len = _strlen((char *)c);
+        write(1, c, s_len);
+        return (s_len);
+}
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: adress of string variable
+ * Return: lenght of srting, 0 if no char exist
+ */
+
+int _strlen(const char *s)
+{
+        int i;
+
+        i = 0;
+
+        while (s[i] != '\0')
+        {
+           i++;
+        }
+
+        return (i);
+}
