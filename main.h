bb566283aa2329ab8497cfe95fbbb9aa9692f1ee
mBentaifour <bentaifourmoh@gmail.com>
2023-11-14 13:46:52 -0800

    update

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int printchar(const char *c, int);
+int printchar(const char *c);
 int printstring(const char *c);
 int _strlen(const char *s);
 int printint(const int *n);
